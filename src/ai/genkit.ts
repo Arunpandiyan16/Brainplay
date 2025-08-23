68e5b7e35326ee9a926146bdcbc6ef6d1b522d59
ARUNPANDIYAN A <aarunpandiyanmec@gmail.com>
2025-08-23 14:58:01 +0000

    Need better way approch the issue , pls fix it

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,19 +1,15 @@
 /**
- * @fileoverview This file initializes and configures the Genkit AI object.
+ * @fileoverview This file initializes and a global AI instance.
  *
  * It sets up the necessary plugins for the application and exports a singleton
  * `ai` object that can be used across the application to define and run
  * AI flows and other Genkit functionalities.
  */
-import { googleAI } from '@genkit-ai/googleai';
-import { genkit } from 'genkit';
+import {googleAI} from '@genkit-ai/googleai';
+import {genkit} from 'genkit';
 
 export const ai = genkit({
-  plugins: [
-    googleAI({
-      apiVersion: 'v1beta',
-    }),
-  ],
+  plugins: [googleAI()],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
